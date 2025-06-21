fb28bba3624de937e65488ad218a6032a6c0db30
Coby <cobysayo@outlook.com>
2025-06-20 18:34:05 -0700

    Changed Fight component props

--- frontend/mmapicks/src/components/FighterCard.tsx
+++ frontend/mmapicks/src/components/FighterCard.tsx
@@ -17,8 +17,7 @@ const FighterCard = () => {
             Lizard
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            STATS
           </Typography>
         </CardContent>
       </CardActionArea>
